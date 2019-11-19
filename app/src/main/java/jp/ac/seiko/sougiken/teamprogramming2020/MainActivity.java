cd1045e634411b8b83081e8099a2902a99e80067
CyberHacDecelop <michael.lee.develop@gmail.com>
2019-11-19 21:24:01 +0900

    Ver 1.0.1 test.

--- app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
+++ app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 博ノ助がきたぞー！アルクェイドかわいい！！
+        // 博ノ助がきたぞー！アルクェイドかわいい！
     }
 
 
