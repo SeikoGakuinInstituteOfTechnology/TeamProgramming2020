35bc0268bb60a5100b7ae31293eddc09a2aa4521
CyberHacDecelop <michael.lee.develop@gmail.com>
2019-11-19 21:08:46 +0900

    Ver 1.0.0 test.

--- app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
+++ app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
@@ -3,12 +3,20 @@ package jp.ac.seiko.sougiken.teamprogramming2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 博ノ助がきたぞー！アルクェイドかわいい！！
+    }
+
+
+    @Override
+    public void onClick (View view) {
+
     }
 }
