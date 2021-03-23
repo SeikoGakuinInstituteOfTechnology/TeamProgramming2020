131bbcae91fdbe889dccc6ddd014f8f6d8d5985b
電脳博ノ助@開発Host <55142566+CyberHacDevelop@users.noreply.github.com>
2021-03-23 22:20:14 +0900

    Update MainActivity.java

--- app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
+++ app/src/main/java/jp/ac/seiko/sougiken/teamprogramming2020/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 博ノ助がきたぞー！アルクェイドかわいい！
         // たぶん僕
+        // は？
         init();
     }
 
